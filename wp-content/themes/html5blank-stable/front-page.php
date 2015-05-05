6b9930c4f11db9e862fc00d54812f460fcc44076
Morgan <mhenry@iquest.fr>
2015-05-05 11:07:20 +0200

    Correction de masonry ..

--- wp-content/themes/html5blank-stable/front-page.php
+++ wp-content/themes/html5blank-stable/front-page.php
@@ -1,16 +1,25 @@
 <?php get_header(); ?>
-<h1>front-page.php</h1>
-<div id="container" class=js-masonry" data-masonry-options='{ "columnWidth": 200, "itemSelector": ".item" }'>
-    <div id="vertical-gutter" class="item"></div>
-    <div id="vertical-gutter" class="item2"></div>
-    <div id="vertical-gutter" class="item"></div>
-    <div id="vertical-gutter" class="item"></div>
-    <div id="vertical-gutter" class="item2"></div>
-    <div id="vertical-gutter" class="item"></div>
-    <div id="vertical-gutter" class="item"></div>
-    <div id="vertical-gutter" class="item2"></div>
-    <div id="vertical-gutter" class="item"></div>
-</div>
-<?php get_template_part ( 'content', 'home' ); ?>
+    <style>
+        /* J'ai géré la taille ici pour pas toucher ton fichier css mais à toi de régler,
+         il faut pas mettre l'image en back ground car masonry calcule la taille à partir de l'image */
+        .item { width: 25%; background:gray;}
+        .item.w2 { width: 50%; }
+    </style>
+    <h1>front-page.php</h1>
+    <!-- t'avais oublié la premiere " après class ...  -->
+    <div id="container" class="js-masonry" data-masonry-options='{ "columnWidth": 200, "itemSelector": ".item" }'>
+        <div  class="item"> <img src="/wp-content/themes/html5blank-stable/img/photo_groupe_enfants_parapente.JPG"/> </div>
+        <!-- Tu dois pas avoir plusierus éléments avec le même id (je sais que tu as fais un copié collé mais bon        -->
+        <div  class="item w2"> <img src="/wp-content/themes/html5blank-stable/img/groupe_enfants.JPG"/> </div>
+        <div  class="item"> <img src="/wp-content/themes/html5blank-stable/img/photo_groupe_enfants_parapente.JPG"/></div>
+        <div  class="item"> <img src="/wp-content/themes/html5blank-stable/img/photo_groupe_enfants_parapente.JPG"/> </div>
+        <div  class="item w2"> <img src="/wp-content/themes/html5blank-stable/img/groupe_enfants.JPG"/></div>
+        <div  class="item"> <img src="/wp-content/themes/html5blank-stable/img/photo_groupe_enfants_parapente.JPG"/></div>
+        <div  class="item"> <img src="/wp-content/themes/html5blank-stable/img/photo_groupe_enfants_parapente.JPG"/> </div>
+        <div  class="item w2"> <img src="/wp-content/themes/html5blank-stable/img/groupe_enfants.JPG"/></div>
+        <div  class="item"> <img src="/wp-content/themes/html5blank-stable/img/photo_groupe_enfants_parapente.JPG"/></div>
+    </div>
+<!-- J'ao viré get template part car ça cherchait qqch que tu voulais pas je crois à remettre sinon -->
+<?php #get_template_part ( 'content', 'home' ); ?>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
